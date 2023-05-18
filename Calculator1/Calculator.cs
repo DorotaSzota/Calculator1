dfb30823be796020601a6a0f0e1114ef41a270da
szotad <szotad@SCSWL4085>
2023-05-18 10:08:32 +0200

    Dodaj pliki projektów.

---
+++ Calculator1/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator1
+{
+    public delegate T Operation<T>(T x, T y);
+    internal class Calculator<T>
+    {
+        private T x;
+        private T y;
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Calculator(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public T PerformOperation(Operation<T> operation)
+        {
+            return operation(x, y);
+        }
+
+    }
+}
---
+++ Calculator1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculator1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation<int> operation1 = (a, b) => a + b;
+            Operation<double> operation2 = (a, b) => a + b;
+            Operation<string> operation3 = (a, b) => a + " " + b +".";
+            var calculator1 = new Calculator<int>(4,6);
+            var calculator2 = new Calculator<double>(3.3, 1.1);
+            var calculator3 = new Calculator<string>("I am", "but a humble creature");
+
+            Console.WriteLine(calculator1.PerformOperation(operation1));
+            Console.WriteLine(calculator2.PerformOperation(operation2));
+            Console.WriteLine(calculator3.PerformOperation(operation3));
+
+        }
+    }
+}
